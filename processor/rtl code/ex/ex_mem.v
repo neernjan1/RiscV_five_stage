26120ed87bb0a9c7efaefe925c664d63688fe404
Neeraj Yadav <131880588+neernjan1@users.noreply.github.com>
2026-03-31 02:06:01 +0530

    Update ex_mem.v

--- processor/rtl code/ex/ex_mem.v
+++ processor/rtl code/ex/ex_mem.v
@@ -2,35 +2,36 @@ module ex_mem (
     input clk,
     input rst,
 
-   
     input [31:0] alu_result_in,
-    input [1:0] mem_in,
-    input wb_in,
-    input branch_in,
-    input [4:0] rd_in,
+    input mem_write_ex,
+    input mem_read_ex,
+    input mem_to_reg_ex,
+    input reg_write_ex,
+    input [4:0] rd_ex,
 
-   
     output reg [31:0] alu_result_out,
-    output reg [1:0] mem_out,
-    output reg wb_out,
-    output reg branch_out,
+    output reg mem_write_mem,
+    output reg mem_read_mem,
+    output reg mem_to_reg_mem,
+    output reg reg_write_mem,
     output reg [4:0] rd_out
 );
 
 always @(posedge clk or posedge rst) begin
     if (rst) begin
         alu_result_out <= 32'b0;
-        mem_out        <= 2'b0;
-        wb_out         <= 1'b0;
-        branch_out     <= 1'b0;
+        mem_write_mem  <= 1'b0;
+        mem_read_mem   <= 1'b0;
+        mem_to_reg_mem <= 1'b0;
+        reg_write_mem  <= 1'b0;
         rd_out         <= 5'b0;
-    end 
-    else begin
+    end else begin
         alu_result_out <= alu_result_in;
-        mem_out        <= mem_in;
-        wb_out         <= wb_in;
-        branch_out     <= branch_in;
-        rd_out         <= rd_in;
+        mem_write_mem  <= mem_write_ex;
+        mem_read_mem   <= mem_read_ex;
+        mem_to_reg_mem <= mem_to_reg_ex;
+        reg_write_mem  <= reg_write_ex;
+        rd_out         <= rd_ex;
     end
 end
 
