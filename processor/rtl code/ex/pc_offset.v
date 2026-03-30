3be9d560f44ad476a8f772baecbafa7387915f6d
Neeraj Yadav <131880588+neernjan1@users.noreply.github.com>
2026-03-30 22:03:16 +0530

    Updated pc_offset.v

--- processor/rtl code/ex/pc_offset.v
+++ processor/rtl code/ex/pc_offset.v
@@ -1,11 +1,10 @@
 module pc_offset(
     input [31:0] pc,
     input [31:0] imm,
-    output reg [31:0] pc_offset_out
+    output wire [31:0] pc_offset_out
 );
 
-always @(*) begin
-    pc_offset_out = pc + imm;
-end
+
+    assign pc_offset_out = pc + imm;
 
 endmodule
