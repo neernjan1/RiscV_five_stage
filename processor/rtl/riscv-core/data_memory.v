dd2d6800a4de909bcc998f9217e8ff874f531e70
Neeraj Yadav <nyadav70096@gmail.com>
2026-04-03 13:37:33 +0530

    different programs are added program,program1,program2

--- processor/rtl/riscv-core/data_memory.v
+++ processor/rtl/riscv-core/data_memory.v
@@ -8,7 +8,7 @@ module data_memory(
     output [31:0] r_data
 );
 
-    reg [31:0] data_mem [0:31];
+    reg [4096:0] data_mem [0:31];
     integer i;
 
     // WRITE + RESET
@@ -17,7 +17,7 @@ module data_memory(
             for (i = 0; i < 4096; i = i + 1)
                 data_mem[i] <= 32'b0;
         end else if (mem_write) begin
-            data_mem[addr[13:0]] <= w_data;
+            data_mem[addr[13:2]] <= w_data;
            
    
         $display("WRITE: MEM[%0d] = %0d", addr[13:0], w_data);
@@ -27,7 +27,7 @@ end
     end
 
     // READ (combinational)
-    assign r_data = mem_read ? data_mem[addr[13:0]] : 32'b0; // addr[13:0] to index 16KB memory 
+    assign r_data = mem_read ? data_mem[addr[13:2]] : 32'b0; // addr[13:0] to index 16KB memory 
     //earlier addr[13:2] for word-aligned access, but we want byte-addressable memory, so we use addr[13:0] to allow access to all 16KB of memory.
     // Note: This simple data memory does not handle unaligned accesses or byte/half-word operations. It assumes all accesses are word-aligned and 32 bits wide.
     // took a lot of time to debug this issue, as I was using addr[13:2] which caused incorrect memory accesses and test failures. Changing to addr[13:0] fixed the problem and allowed proper access to the entire 16KB memory space.
