8462da67bb8bdafe3d1b6c3f3bde0e6a960ea49d
Irfan3066 <158747291+Irfan3066@users.noreply.github.com>
2026-04-04 17:38:37 +0530

    Update mux_wb.v

--- processor/rtl/riscv-core/mux_wb.v
+++ processor/rtl/riscv-core/mux_wb.v
@@ -1,10 +1,40 @@
 `timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/28/2026 11:48:36 AM
+// Design Name: 
+// Module Name: mux_wb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
 
 module mux_wb(
 input [31:0] alu_result ,
 input [31:0] r_data ,
-input mem_to_reg ,
-output [31:0] data_out 
+input [31:0] pc_plus_4_wb,// 🔥 NEW
+input [1:0] result_src, // 🔥 NEW
+
+
+output reg [31:0] data_out 
     );
-    assign data_out = (mem_to_reg == 1'b1) ? r_data : alu_result;
+    always @(*) begin// 🔥 NEW
+    case(result_src)
+        2'b00: data_out = alu_result;
+        2'b01: data_out = r_data;
+        2'b10: data_out = pc_plus_4_wb;  // 🔥 JAL/JALR
+        default: data_out = alu_result;
+    endcase
+end
 endmodule
