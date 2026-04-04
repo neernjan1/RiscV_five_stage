62e2047558b4c7d8560c92d1a2dd1a3618aefd81
Irfan3066 <158747291+Irfan3066@users.noreply.github.com>
2026-04-04 17:37:09 +0530

    Update ex_mem.v

--- processor/rtl/riscv-core/ex_mem.v
+++ processor/rtl/riscv-core/ex_mem.v
@@ -21,7 +21,13 @@ module ex_mem (
     output reg mem_read_out,
     output reg mem_write_out,
     output reg mem_to_reg_out,
-    output reg reg_write_out
+    output reg reg_write_out , 
+    
+    input  [31:0] pc_plus_4_ex, // 🔥 NEW
+    output reg [31:0] pc_plus_4_mem , // 🔥 NEW
+    // 🔥 ADD
+    input [1:0] result_src_ex,// 🔥 NEW
+    output reg [1:0] result_src_mem// 🔥 NEW
 );
 
 always @(posedge clk) begin
@@ -34,6 +40,8 @@ always @(posedge clk) begin
         mem_write_out <= 0;
         mem_to_reg_out <= 0;
         reg_write_out <= 0;
+        pc_plus_4_mem <= 0 ;// 🔥 NEW
+        result_src_mem <= 0 ;// 🔥 NEW
     end
     else begin
         alu_result_out <= alu_result_in;
@@ -44,6 +52,8 @@ always @(posedge clk) begin
         mem_write_out <= mem_write_in;
         mem_to_reg_out <= mem_to_reg_in;
         reg_write_out <= reg_write_in;
+        pc_plus_4_mem <= pc_plus_4_ex ;// 🔥 NEW
+        result_src_mem <= result_src_ex ; // 🔥 NEW
     end
 end
 
