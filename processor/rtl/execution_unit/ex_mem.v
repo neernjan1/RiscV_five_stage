873b26de381523a79404daa6a98484ce373175c6
Irfan3066 <158747291+Irfan3066@users.noreply.github.com>
2026-04-04 17:19:42 +0530

    Update ex_mem.v

--- processor/rtl/execution_unit/ex_mem.v
+++ processor/rtl/execution_unit/ex_mem.v
@@ -14,7 +14,12 @@ module ex_mem (
     output reg mem_read_mem,
     output reg mem_to_reg_mem,
     output reg reg_write_mem,
-    output reg [4:0] rd_out
+    output reg [4:0] rd_out ,
+     input  [31:0] pc_plus_4_ex, // 🔥 NEW
+    output reg [31:0] pc_plus_4_mem , // 🔥 NEW
+    // 🔥 ADD
+    input [1:0] result_src_ex,// 🔥 NEW
+    output reg [1:0] result_src_mem// 🔥 NEW
 );
 
 always @(posedge clk or posedge rst) begin
@@ -25,6 +30,8 @@ always @(posedge clk or posedge rst) begin
         mem_to_reg_mem <= 1'b0;
         reg_write_mem  <= 1'b0;
         rd_out         <= 5'b0;
+         pc_plus_4_mem <= 0 ;// 🔥 NEW
+        result_src_mem <= 0 ;// 🔥 NEW
     end else begin
         alu_result_out <= alu_result_in;
         mem_write_mem  <= mem_write_ex;
@@ -32,6 +39,8 @@ always @(posedge clk or posedge rst) begin
         mem_to_reg_mem <= mem_to_reg_ex;
         reg_write_mem  <= reg_write_ex;
         rd_out         <= rd_ex;
+         pc_plus_4_mem <= pc_plus_4_ex ;// 🔥 NEW
+        result_src_mem <= result_src_ex ; // 🔥 NEW
     end
 end
 
